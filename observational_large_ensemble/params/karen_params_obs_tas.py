5f3e31294f4dea4c2dee576abf153189207167af
Talia <taliaanderson@email.arizona.edu>
2022-08-17 12:36:04 -0700

    temp params

---
+++ observational_large_ensemble/params/karen_params_obs_tas.py
@@ -0,0 +1,19 @@
+"""Example parameter file for Observational Large Ensemble code."""
+
+import numpy as np
+
+version_name = 'CLLJ_CentAm_T'
+valid_years = np.arange(1920, 2020)  # for obs
+latbounds = [28, 6] # for obs (covers Caribbean and Central America)
+lonbounds = [-100, -60] # for obs
+cvdp_loc = '/glade/work/tanderson/CVDP'
+AMO_cutoff_freq = 1/20  # Cut off frequency for Butterworth filter of AMO (1/years)
+mode_lag = 0  # number of months to lag between mode time series and climate response
+workdir_base = '/glade/work/tanderson/obsLE/parameters_v-%s' % version_name
+output_dir = '/glade/scratch/tanderson/obsLE/output_v-%s' % version_name
+tas_dir = '/glade/work/tanderson/BEST'
+pr_dir = '/glade/work/tanderson/GPCC'
+slp_dir = '/glade/work/mckinnon/20CRv2c'
+pr_transform = 'boxcox'  # can be boxcox or log
+varnames = ['tas']
+predictors_names = ['constant', 'F', 'ENSO', 'PDO_orth', 'AMO_lowpass', 'CLLJ']
